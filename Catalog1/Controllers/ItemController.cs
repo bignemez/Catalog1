ce7963f55c366c5ff4c4b7d36b072ec08d1ce7da
En Dru <En.Dru@westen.de>
2021-07-06 15:01:53 +0200

    Projektdateien hinzufügen.

---
+++ Catalog1/Controllers/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using Catalog1.Entities;
+using Catalog1.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Catalog1.Controllers
+{
+    [ApiController]
+    [Route("Items")]
+    public class ItemController
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return repository.GetItems();
+        }
+    }
+}
---
+++ Catalog1/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,26 @@
+﻿using Catalog1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Catalog1.Repositories
+{
+    public class InMemItemsRepository
+    {
+        List<Item> items = new()
+        {
+            new Item { Id=Guid.NewGuid(),Name="Poison",Price=9,CreatedDate=DateTimeOffset.Now},
+            new Item { Id=Guid.NewGuid(),Name="Iron Sword",Price=19,CreatedDate=DateTimeOffset.Now},
+            new Item { Id=Guid.NewGuid(),Name="Silver Shield",Price=14,CreatedDate=DateTimeOffset.Now},
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
