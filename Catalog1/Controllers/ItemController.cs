e644160f30ed727ea63e5b6c93950d0bd801195a
En Dru <En.Dru@westen.de>
2021-07-06 15:25:52 +0200

    bis zu 40 minute

--- Catalog1/Controllers/ItemController.cs
+++ Catalog1/Controllers/ItemController.cs
@@ -1,13 +1,14 @@
 ﻿using Catalog1.Entities;
 using Catalog1.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace Catalog1.Controllers
 {
     [ApiController]
     [Route("Items")]
-    public class ItemController
+    public class ItemController:ControllerBase
     {
         private readonly InMemItemsRepository repository;
 
@@ -16,10 +17,23 @@ namespace Catalog1.Controllers
             repository = new InMemItemsRepository();
         }
 
+        // GET /items
         [HttpGet]
         public IEnumerable<Item> GetItems()
         {
             return repository.GetItems();
+        }
+
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+           var item= repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
         }
     }
 }
